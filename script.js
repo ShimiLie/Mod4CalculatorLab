d48bf7c8acae8962ea9167bc875e77b90f7334a0
alie <anthonylie94@gmail.com>
2023-02-04 17:00:12 +0800

    added all operators

--- script.js
+++ script.js
@@ -10,14 +10,35 @@ function addition() {
     console.log(value);
 };
 
+function subtraction() {
+    let first = document.getElementById("firstNum").value;
+    let second = document.getElementById("secondNum").value;
+    let value = parseInt(first) - parseInt(second);
 
+    document.getElementById("value").innerHTML = "=" + value  ;
+    console.log(first);
+    console.log(second);
+    console.log(value);
+};
+
+
+function divide() {
+    let first = document.getElementById("firstNum").value;
+    let second = document.getElementById("secondNum").value;
+    let value = parseInt(first) / parseInt(second);
+    
+    document.getElementById("value").innerHTML = "=" + value  ;
+    console.log(first);
+    console.log(second);
+    console.log(value);
+};
 
 function multiply() {
     let first = document.getElementById("firstNum").value;
     let second = document.getElementById("secondNum").value;
     let value = parseInt(first) * parseInt(second);
     
-    document.getElementById("value").innerHTML = "=" + "" + value  ;
+    document.getElementById("value").innerHTML = "=" + value  ;
     console.log(first);
     console.log(second);
     console.log(value);
