f369cb682ff819e398c395c9c9a036812a3566d3
alie <anthonylie94@gmail.com>
2023-02-04 13:47:04 +0800

    created addition function

---
+++ script.js
@@ -0,0 +1,11 @@
+
+function addition() {
+    let first = document.getElementById("firstNum").value;
+    let second = document.getElementById("secondNum").value;
+    let value = parseInt(first) + parseInt(second);
+    
+    document.getElementById("value").innerHTML = "=" + value  ;
+    console.log(first);
+    console.log(second);
+    console.log(value);
+};
