23471c587f448dc1ce1de34731b78f87e510eac8
alie <anthonylie94@gmail.com>
2023-02-04 16:37:31 +0800

    added division

--- script.js
+++ script.js
@@ -9,3 +9,16 @@ function addition() {
     console.log(second);
     console.log(value);
 };
+
+
+
+function divide() {
+    let first = document.getElementById("firstNum").value;
+    let second = document.getElementById("secondNum").value;
+    let value = parseInt(first) / parseInt(second);
+    
+    document.getElementById("value").innerHTML = "=" + value  ;
+    console.log(first);
+    console.log(second);
+    console.log(value);
+};
